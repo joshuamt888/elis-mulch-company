411e0d00fba925ae08f66ca9eab89b6c59ef29b8
Joshua Montanez <josh@steadyscaling.com>
2026-03-25 16:48:36 -0500

    Add Microsoft Clarity analytics (w1hg4fl8c5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -179,6 +179,13 @@ export default function RootLayout({
           gtag('js', new Date());
           gtag('config', 'G-NBWTEY1D7R');
         `}</Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">{`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "w1hg4fl8c5");
+        `}</Script>
       </head>
       <body className={`${inter.variable} ${outfit.variable} antialiased`}>
         {children}
